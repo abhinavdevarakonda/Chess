069fb6f95004c7659de1e9bae37f573e97dc07f8
abhinavdevarakonda <50144533+abhinavdevarakonda@users.noreply.github.com>
2022-04-14 23:57:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,33 @@ import time
 import pygame
 import math
 
-#black_pawn = bp          #white_pawn = wp
-#black_knight = bk        #white_knight = bk
-#black_bishop = bb        #white_bishop = wb
-#black_rook = br          #white_rook = wr
-#black_queen = bqueen     #white_queen = wqueen
-#black_king = bking       #white_king = wking
+pygame.init()
+GameDisplay = pygame.display.set_mode((800,800))
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+square_size = 80
+GameDisplay.fill(WHITE)
+
+k=0
+for i in range(1,9):
+    for j in range(1,9):
+        if k%2 == 0:
+            pygame.draw.rect(GameDisplay,WHITE,(j*square_size,i*square_size,square_size,square_size))
+        else:
+            pygame.draw.rect(GameDisplay,BLACK,(j*square_size,i*square_size,square_size,square_size))
+        k+=1
+    k-=1
+
+pygame.display.set_caption('CHESS')
+Clock = pygame.time.Clock()
+
+#black_pawn      #white_pawn 
+#black_knight    #white_knight 
+#black_bishop    #white_bishop 
+#black_rook      #white_rook 
+#black_queen     #white_queen 
+#black_king      #white_king 
 
 
 def Board():
@@ -42,4 +63,12 @@ def rook_move(initial_x,initial_y,final_x,final_y):
         move(initial_x,initial_y,final_x,final_y)
 def queen_move(initial_x,initial_y,final_x,final_y):
     if (initial_x==final_x) or (initial_y==final_y):
+        pass
 
+
+end = False
+while not end:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            end = True
+    pygame.display.update()
