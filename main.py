bbb49caf857792175ae6ce94265885526e0d7da4
rudrakshpadhi <rudraksh.padhi@gmail.com>
2022-04-14 22:53:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 import time
 import pygame
 
-#black_pawn      #white_pawn 
-#black_knight    #white_knight 
-#black_bishop    #white_bishop 
-#black_rook      #white_rook 
-#black_queen     #white_queen 
-#black_king      #white_king 
+#black_pawn = bp          #white_pawn = wp
+#black_knight = bk        #white_knight = bk
+#black_bishop = bb        #white_bishop = wb
+#black_rook = br          #white_rook = wr
+#black_queen = bqueen     #white_queen = wqueen
+#black_king = bking       #white_king = wking
 
 
 def Board():
-    board = [8]
-    for i in range(2,5):
-        for j in range(8):
-            board[i][j]=' '
+    board = []
+    for i in range(8):
+        board.append([' ',' ',' ',' ',' ',' ',' ',' '])
     
     for i in range(8):
-        board[1][i] = 'white_pawn'
-        board[6][i] = 'black_pawn'
+        board[1][i] = 'wp'
+        board[6][i] = 'bp'
     
-    board[0]=['white_rook','white_knight','white_bishop','white_queen','white_king','white_bishop','white_knight','white_rook']
-    board[7]=['black_rook','black_knight','black_bishop','black_queen','black_king','black_bishop','black_knight','black_rook']
-    print(board)
+    board[0] = ['wr','wk','wb','wking','wqueen','wb','wk','wr']
+    board[7] = ['br','bk','bb','bking','bqueen','bb','bk','br']
+    for i in range(8):
+        print(board[i],"\n")
 Board()
-
 
