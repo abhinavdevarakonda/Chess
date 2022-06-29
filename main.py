e422a1526b1ce74d9991a8c59d88d66b66d1bc08
rudrakshpadhi <rudraksh.padhi@gmail.com>
2022-06-29 16:54:16 +0530

    pawn movement done

--- main.py
+++ main.py
@@ -155,10 +155,20 @@ def move(initial_coords,final_coords):
         board[initial_x][initial_y] = ' '
         Board()
 
-    def pawn_first_move(initial_x,initial_y,final_x,final_y):
-        print(math.fabs(final_x-initial_x))
-        if initial_y==final_y and (math.fabs(final_x-initial_x)==1 or math.fabs(final_x-initial_x)==2):
-            print('here')
+    def white_pawn_first_move(initial_x,initial_y,final_x,final_y):
+        if initial_y==final_y and ((final_x-initial_x)==-1 or (final_x-initial_x)==-2):
+            movement(initial_x,initial_y,final_x,final_y)
+    
+    def white_pawn_move(initial_x,initial_y,final_x,final_y):
+        if (initial_y==final_y) and (final_x-initial_x==-1):
+            movement(initial_x,initial_y,final_x,final_y)
+    
+    def black_pawn_first_move(initial_x,initial_y,final_x,final_y):
+        if initial_y==final_y and ((final_x-initial_x)==1 or (final_x-initial_x)==2):
+            movement(initial_x,initial_y,final_x,final_y)
+    
+    def black_pawn_move(initial_x,initial_y,final_x,final_y):
+        if (initial_y==final_y) and (final_x-initial_x==1):
             movement(initial_x,initial_y,final_x,final_y)
 
     def knight_move(initial_x,initial_y,final_x,final_y):
@@ -178,8 +188,17 @@ def move(initial_coords,final_coords):
             movement(initial_x,initial_y,final_x,final_y)
     
     #if pawn first move
-    if board[initial_x][initial_y][1] == 'p':
-        pawn_first_move(initial_x,initial_y,final_x,final_y)
+    if board[initial_x][initial_y][0] == 'w' and board[initial_x][initial_y][1] == 'p' and initial_x==6:
+        white_pawn_first_move(initial_x,initial_y,final_x,final_y)
+    
+    elif board[initial_x][initial_y][0] == 'b' and board[initial_x][initial_y][1] == 'p'and initial_x==1:
+        black_pawn_first_move(initial_x,initial_y,final_x,final_y)\
+    
+    elif board[initial_x][initial_y][0] == 'w' and board[initial_x][initial_y][1] == 'p'and initial_x!=6:
+        white_pawn_move(initial_x,initial_y,final_x,final_y)
+    
+    elif board[initial_x][initial_y][0] == 'b' and board[initial_x][initial_y][1] == 'p'and initial_x!=1:
+        black_pawn_move(initial_x,initial_y,final_x,final_y)
     
     elif board[initial_x][initial_y][1] == 'r':
         rook_move(initial_x,initial_y,final_x,final_y)
