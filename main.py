2f5e49365928729a60b6dd9f7bca10e590264059
abhinavdevarakonda <50144533+abhinavdevarakonda@users.noreply.github.com>
2022-07-01 12:51:20 +0530

    added path feature
    moves piece only if the path it has to take is empty and fixed turn system

--- main.py
+++ main.py
@@ -6,73 +6,71 @@ HEIGHT = 800
 WIDTH = 800
 pygame.init()
 
-GameDisplay = pygame.display.set_mode((HEIGHT,WIDTH))
+GameDisplay = pygame.display.set_mode((800,800))
 
 #############INITIAL BOARD DRAWING######################
 WHITE = (255,255,255)
 BLACK = (165,42,42)
 square_size = 80
 GameDisplay.fill(WHITE)
-
+'''
 k=0
 for i in range(1,9):
     for j in range(1,9):
+        j += 2 #just to shift the whole board a little to the right
         if k%2 == 0:
-            pygame.draw.rect(GameDisplay,WHITE,(j*square_size,i*square_size,square_size,square_size))
+            pygame.draw.rect(GameDisplay,WHITE,((j)*square_size,i*square_size,square_size,square_size))
         else:
-            pygame.draw.rect(GameDisplay,BLACK,(j*square_size,i*square_size,square_size,square_size))
+            pygame.draw.rect(GameDisplay,BLACK,((j)*square_size,i*square_size,square_size,square_size))
         k+=1
     k-=1
-
+'''
 pygame.display.set_caption('CHESS')
+
 black_rook_img=pygame.image.load('pieces/black_rook.png')
 black_rook_img=pygame.transform.scale(black_rook_img,(80,80))
-GameDisplay.blit(black_rook_img,(HEIGHT/10,WIDTH/10))
-GameDisplay.blit(black_rook_img,(8*HEIGHT/10,WIDTH/10))
+#GameDisplay.blit(black_rook_img,(HEIGHT/10,WIDTH/10))
+#GameDisplay.blit(black_rook_img,(8*HEIGHT/10,WIDTH/10))
 black_knight_img=pygame.image.load('pieces/black_knight.png')
 black_knight_img=pygame.transform.scale(black_knight_img,(80,80))
-GameDisplay.blit(black_knight_img,(2*HEIGHT/10,WIDTH/10))
-GameDisplay.blit(black_knight_img,(7*HEIGHT/10,WIDTH/10))
+#GameDisplay.blit(black_knight_img,(2*HEIGHT/10,WIDTH/10))
+#GameDisplay.blit(black_knight_img,(7*HEIGHT/10,WIDTH/10))
 black_bishop_img=pygame.image.load('pieces/black_bishop.png')
 black_bishop_img=pygame.transform.scale(black_bishop_img,(80,80))
-GameDisplay.blit(black_bishop_img,(3*HEIGHT/10,WIDTH/10))
-GameDisplay.blit(black_bishop_img,(6*HEIGHT/10,WIDTH/10))
+#GameDisplay.blit(black_bishop_img,(3*HEIGHT/10,WIDTH/10))
+#GameDisplay.blit(black_bishop_img,(6*HEIGHT/10,WIDTH/10))
 black_king_img=pygame.image.load('pieces/black_king.png')
 black_king_img=pygame.transform.scale(black_king_img,(80,80))
-GameDisplay.blit(black_king_img,(5*HEIGHT/10,WIDTH/10))
+#GameDisplay.blit(black_king_img,(5*HEIGHT/10,WIDTH/10))
 black_queen_img=pygame.image.load('pieces/black_queen.png')
 black_queen_img=pygame.transform.scale(black_queen_img,(80,80))
-GameDisplay.blit(black_queen_img,(4*HEIGHT/10,WIDTH/10))
-i=1
-while i<=8:
-    black_pawn_img=pygame.image.load('pieces/black_pawn.png')
-    black_pawn_img=pygame.transform.scale(black_pawn_img,(80,80))
-    GameDisplay.blit(black_pawn_img,(i*HEIGHT/10,2*WIDTH/10))
-    i=i+1
+#GameDisplay.blit(black_queen_img,(4*HEIGHT/10,WIDTH/10))
+
+black_pawn_img=pygame.image.load('pieces/black_pawn.png')
+black_pawn_img=pygame.transform.scale(black_pawn_img,(80,80))
+#GameDisplay.blit(black_pawn_img,(i*HEIGHT/10,2*WIDTH/10))
 white_rook_img=pygame.image.load('pieces/white_rook.png')
 white_rook_img=pygame.transform.scale(white_rook_img,(80,80))
-GameDisplay.blit(white_rook_img,(HEIGHT/10,8*WIDTH/10))
-GameDisplay.blit(white_rook_img,(8*HEIGHT/10,8*WIDTH/10))
+#GameDisplay.blit(white_rook_img,(HEIGHT/10,8*WIDTH/10))
+#GameDisplay.blit(white_rook_img,(8*HEIGHT/10,8*WIDTH/10))
 white_knight_img=pygame.image.load('pieces/white_knight.png')
 white_knight_img=pygame.transform.scale(white_knight_img,(80,80))
-GameDisplay.blit(white_knight_img,(2*HEIGHT/10,8*WIDTH/10))
-GameDisplay.blit(white_knight_img,(7*HEIGHT/10,8*WIDTH/10))
+#GameDisplay.blit(white_knight_img,(2*HEIGHT/10,8*WIDTH/10))
+#GameDisplay.blit(white_knight_img,(7*HEIGHT/10,8*WIDTH/10))
 white_bishop_img=pygame.image.load('pieces/white_bishop.png')
 white_bishop_img=pygame.transform.scale(white_bishop_img,(80,80))
-GameDisplay.blit(white_bishop_img,(3*HEIGHT/10,8*WIDTH/10))
-GameDisplay.blit(white_bishop_img,(6*HEIGHT/10,8*WIDTH/10))
+#GameDisplay.blit(white_bishop_img,(3*HEIGHT/10,8*WIDTH/10))
+#GameDisplay.blit(white_bishop_img,(6*HEIGHT/10,8*WIDTH/10))
 white_king_img=pygame.image.load('pieces/white_king.png')
 white_king_img=pygame.transform.scale(white_king_img,(80,80))
-GameDisplay.blit(white_king_img,(5*HEIGHT/10,8*WIDTH/10))
+#GameDisplay.blit(white_king_img,(5*HEIGHT/10,8*WIDTH/10))
 white_queen_img=pygame.image.load('pieces/white_queen.png')
 white_queen_img=pygame.transform.scale(white_queen_img,(80,80))
-GameDisplay.blit(white_queen_img,(4*HEIGHT/10,8*WIDTH/10))
-i=1
-while i<=8:
-    white_pawn_img=pygame.image.load('pieces/white_pawn.png')
-    white_pawn_img=pygame.transform.scale(white_pawn_img,(80,80))
-    GameDisplay.blit(white_pawn_img,(i*HEIGHT/10,7*WIDTH/10))
-    i=i+1
+#GameDisplay.blit(white_queen_img,(4*HEIGHT/10,8*WIDTH/10))
+
+white_pawn_img=pygame.image.load('pieces/white_pawn.png')
+white_pawn_img=pygame.transform.scale(white_pawn_img,(80,80))
+#GameDisplay.blit(white_pawn_img,(i*HEIGHT/10,7*WIDTH/10))
 
 
 
@@ -138,7 +136,7 @@ def Board():
                 GameDisplay.blit(white_king_img,((j+1)*HEIGHT/10,(i+1)*WIDTH/10))
             
             elif board[i][j][:2] == 'bp':
-                GameDisplay.blit(black_pawn_img,((j+1)*HEIGHT/10,(i+1)*WIDTH/10))
+                GameDisplay.blit(black_pawn_img,((j+1)*HEIGHT/10,(i+1)*WIDTH/10))   
             elif board[i][j][:2] == 'wp':
                 GameDisplay.blit(white_pawn_img,((j+1)*HEIGHT/10,(i+1)*WIDTH/10))
 
@@ -150,26 +148,204 @@ def move(initial_coords,final_coords):
     final_x = final_coords[0]
     final_y = final_coords[1]
 
+    def IsEmpty(initial_x,initial_y,final_x,final_y):
+        path = []
+        piece = board[initial_x][initial_y][1:]
+        valid = True
+        def horizontal_right(ix,iy,fx,fy):
+            #ix=fx,iy<fy
+            for y in range(iy+1,fy+1):
+                path.append(board[ix][y])
+            return path
+        def horizontal_left(ix,iy,fx,fy):
+            #ix=fx,iy>fy
+            for y in range(iy-1,fy-1,-1):
+                path.append(board[ix][y])
+            return path
+        def vertical_up(ix,iy,fx,fy):
+            #ix>fx,iy=fy
+            for x in range(ix-1,fx-1,-1):
+                path.append(board[x][iy])
+            return path
+        def vertical_down(ix,iy,fx,fy):
+            #ix<fx,iy=fy
+            for x in range(ix+1,fx+1):
+                path.append(board[x][iy])
+            return path
+        def diagonal_upright(ix,iy,fx,fy):
+            #ix>fx,iy<fy
+            x = ix
+            x-=1
+            for y in range(iy+1,fy+1):
+                path.append(board[x][y])
+                x-=1
+            return path
+        def diagonal_upleft(ix,iy,fx,fy):
+            #ix>fx,iy>fy
+            x = ix
+            x-=1
+            for y in range(iy-1,fy-1,-1):
+                path.append(board[x][y])
+                x-=1
+            return path
+        def diagonal_downright(ix,iy,fx,fy):
+            #ix<fx,iy<fy
+            x=ix
+            x+=1
+            for y in range(iy+1,fy+1):
+                path.append(board[x][y])
+                x+=1
+            return path
+        def diagonal_downleft(ix,iy,fx,fy):
+            #ix<fx,iy>fy
+            x = ix
+            x+=1
+            for y in range(iy-1,fy-1,-1):
+                path.append(board[x][y])
+                x+=1
+            return path
+        
+        if piece == 'kn':
+            return True
+
+        if piece == 'p':
+            if initial_x < final_x:
+                for i in range(initial_x+1,final_x+1):
+                    path.append(board[i][initial_y])
+            else:
+                
+                for i in range(initial_x-1,final_x-1,-1):
+                    path.append(board[i][initial_y])
+
+            for i in path:
+                if i != ' ':
+                    valid = False
+        
+        elif piece == 'r1' or piece == 'r2':
+            if initial_x == final_x:
+                if initial_y < final_y:
+                    for i in horizontal_right(initial_x,initial_y,final_x,final_y):
+                        if i != ' ':
+                            valid = False
+                elif initial_y > final_y:
+                    for i in horizontal_left(initial_x,initial_y,final_x,final_y):
+                        if i != ' ':
+                            valid = False
+            elif initial_y == final_y:
+                if initial_x < final_x:
+                    for i in vertical_down(initial_x,initial_y,final_x,final_y):
+                        if i != ' ':
+                            valid = False
+                elif initial_x > final_x:
+                    for i in vertical_up(initial_x,initial_y,final_x,final_y):
+                        if i != ' ':
+                            valid = False
+
+        elif piece == 'b1' or piece == 'b2':
+            #up
+            if initial_x > final_x:
+                #left
+                if initial_y > final_y:
+                    for i in diagonal_upleft(initial_x,initial_y,final_x,final_y):
+                        if i != ' ':
+                            valid = False
+                #right
+                elif initial_y < final_y:
+                    for i in diagonal_upright(initial_x,initial_y,final_x,final_y):
+                        if i != ' ':
+                            valid = False
+            #down
+            elif initial_x < final_x:
+                #left
+                if initial_y > final_y:
+                    for i in diagonal_downleft(initial_x,initial_y,final_x,final_y):
+                        if i != ' ':
+                            valid = False
+                #right
+                elif initial_y < final_y:
+                    for i in diagonal_downright(initial_x,initial_y,final_x,final_y):
+                        if i != ' ':
+                            valid = False
+        elif piece == 'q' or 'k':
+            #horizontal
+            if initial_x == final_x:
+                #right
+                if initial_y < final_y:
+                    for i in horizontal_right(initial_x,initial_y,final_x,final_y):
+                        if i != ' ':
+                            valid = False
+                elif initial_y > final_y:
+                    #left
+                    for i in horizontal_left(initial_x,initial_y,final_x,final_y):
+                        if i != ' ':
+                            valid = False
+            elif initial_y == final_y:
+                #down
+                if initial_x < final_x:
+                    for i in vertical_down(initial_x,initial_y,final_x,final_y):
+                        if i != ' ':
+                            valid = False
+                #up
+                elif initial_x > final_x:
+                    for i in vertical_up(initial_x,initial_y,final_x,final_y):
+                        if i != ' ':
+                            valid = False
+
+            #up
+            elif initial_x > final_x:
+                #left
+                if initial_y > final_y:
+                    for i in diagonal_upleft(initial_x,initial_y,final_x,final_y):
+                        if i!= ' ':
+                            valid = False
+                #right
+                elif initial_y < final_y:
+                    print('here')
+                    for i in diagonal_upright(initial_x,initial_y,final_x,final_y):
+                        if i != ' ':
+                            valid = False
+            #down
+            elif initial_x < final_x:
+                #left
+                if initial_y > final_y:
+                    for i in diagonal_downleft(initial_x,initial_y,final_x,final_y):
+                        if i != ' ':
+                            valid = False
+                #right
+                elif initial_y < final_y:
+                    for i in diagonal_downright(initial_x,initial_y,final_x,final_y):
+                        if i != ' ':
+                            valid = False
+        print(path,'...path')
+        print(valid,'..valid')
+        return valid
+            
     def movement(initial_x,initial_y,final_x,final_y):
+        global turns
         board[final_x][final_y] = MovingPiece
         board[initial_x][initial_y] = ' '
         Board()
+        turns+=1
 
     def white_pawn_first_move(initial_x,initial_y,final_x,final_y):
-        if initial_y==final_y and ((final_x-initial_x)==-1 or (final_x-initial_x)==-2):
-            movement(initial_x,initial_y,final_x,final_y)
+        if (initial_y==final_y and ((final_x-initial_x)==-1 or (final_x-initial_x)==-2)):
+            if IsEmpty(initial_x, initial_y, final_x, final_y):
+                movement(initial_x,initial_y,final_x,final_y)
     
     def white_pawn_move(initial_x,initial_y,final_x,final_y):
         if (initial_y==final_y) and (final_x-initial_x==-1):
-            movement(initial_x,initial_y,final_x,final_y)
+            if IsEmpty(initial_x, initial_y, final_x, final_y):
+                movement(initial_x,initial_y,final_x,final_y)
     
     def black_pawn_first_move(initial_x,initial_y,final_x,final_y):
         if initial_y==final_y and ((final_x-initial_x)==1 or (final_x-initial_x)==2):
-            movement(initial_x,initial_y,final_x,final_y)
+            if IsEmpty(initial_x, initial_y, final_x, final_y):
+                movement(initial_x,initial_y,final_x,final_y)
     
     def black_pawn_move(initial_x,initial_y,final_x,final_y):
         if (initial_y==final_y) and (final_x-initial_x==1):
-            movement(initial_x,initial_y,final_x,final_y)
+            if IsEmpty(initial_x, initial_y, final_x, final_y):
+                movement(initial_x,initial_y,final_x,final_y)
 
     def knight_move(initial_x,initial_y,final_x,final_y):
         if (math.fabs(final_x-initial_x)==1 and math.fabs(final_y-initial_y)==2)or(math.fabs(final_x-initial_x)==2 and math.fabs(final_y-initial_y)==1):
@@ -177,19 +353,24 @@ def move(initial_coords,final_coords):
 
     def bishop_move(initial_x,initial_y,final_x,final_y):
         if (math.fabs(final_x-initial_x)==math.fabs(final_y-initial_y)):
-            movement(initial_x,initial_y,final_x,final_y)
+            if IsEmpty(initial_x, initial_y, final_x, final_y):
+                movement(initial_x,initial_y,final_x,final_y)
 
     def rook_move(initial_x,initial_y,final_x,final_y):
         if (initial_x==final_x) or (initial_y==final_y):
-            movement(initial_x,initial_y,final_x,final_y)
+            if IsEmpty(initial_x, initial_y, final_x, final_y):
+                movement(initial_x,initial_y,final_x,final_y)
 
     def queen_move(initial_x,initial_y,final_x,final_y):
         if ((initial_x==final_x) or (initial_y==final_y)) or ((math.fabs(final_x-initial_x)==math.fabs(final_y-initial_y))):
-            movement(initial_x,initial_y,final_x,final_y)
+            if IsEmpty(initial_x, initial_y, final_x, final_y):
+                movement(initial_x,initial_y,final_x,final_y)
+
     def king_move(initial_x,initial_y,final_x,final_y):
         if((math.fabs(final_x-initial_x)==1 and math.fabs(final_y-initial_y)==1)or(math.fabs(final_x-initial_x)==0 and math.fabs(final_y-initial_y)==1)or(math.fabs(final_x-initial_x)==1 and math.fabs(final_y-initial_y)==0)):
-            movement(initial_x,initial_y,final_x,final_y)
-    
+            if IsEmpty(initial_x, initial_y, final_x, final_y):
+                movement(initial_x,initial_y,final_x,final_y)
+
     #if pawn first move
     if board[initial_x][initial_y][0] == 'w' and board[initial_x][initial_y][1] == 'p' and initial_x==6:
         white_pawn_first_move(initial_x,initial_y,final_x,final_y)
@@ -217,7 +398,7 @@ def move(initial_coords,final_coords):
     
     elif board[initial_x][initial_y][1] == 'k'and len(board[initial_x][initial_y])==2:
         king_move(initial_x,initial_y,final_x,final_y)
-
+    
     
         
 
@@ -231,12 +412,14 @@ def ClickedSquare(coordinates):
 running = True
 step = 0
 turns = 0
+Board()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         
         if event.type == pygame.MOUSEBUTTONUP:
+            #print(turns)
             mouse_position = pygame.mouse.get_pos()
             clicked_coords = ClickedSquare(mouse_position)
 
@@ -246,7 +429,6 @@ while running:
                 if clicked_coords[0] < 0 or clicked_coords[1] < 0:
                     pass
                 elif item != ' ' and step==0:
-                    print(turns,'...turns')
                     if (turns%2==0 and board[clicked_coords[0]][clicked_coords[1]][0] == 'w') or (turns%2==1 and board[clicked_coords[0]][clicked_coords[1]][0] == 'b'):
                         initial_coordinates = clicked_coords
                         MovingPiece = board[clicked_coords[0]][clicked_coords[1]]
@@ -260,14 +442,15 @@ while running:
                         #this is if they try to capture a piece (click on their piece and then the enemy piece)
                         print(board[initial_coordinates[0]][initial_coordinates[1]],'...initial')
                         print(board[clicked_coords[0]][clicked_coords[1]],'...final')
-                        move(initial_coordinates,clicked_coords)
+                        #move(initial_coordinates,clicked_coords)
                         step = 0
                         turns += 1
 
                 elif item == ' ' and step == 1:
-                    move(initial_coordinates,clicked_coords)
+                    move(initial_coordinates,clicked_coords) 
                     step = 0
-                    turns += 1
+                    print(turns)
+                    #turns += 1
                 
             except IndexError:
                 pass
